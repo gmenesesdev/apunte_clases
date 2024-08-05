c5801e2f82218dd4533e952713badb1b11d17f9d
Gonzalo Meneses <gonzalo.menesesg@gmail.com>
2024-08-05 15:49:53 -0400

    Se modifica app.blade para agregar configuraciones de Jquery, Bootstrap, AdminLTE y Custom JS

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -7,6 +7,8 @@
     <title>@yield('title')</title>
     <!-- AdminLTE CSS -->
     <link rel="stylesheet" href="{{ asset('dist/css/adminlte.min.css') }}">
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
     <!-- Font Awesome CSS -->
     <link rel="stylesheet" href="{{ asset('plugins/fontawesome-free/css/all.min.css') }}">
     <!--Custom CSS-->
@@ -45,6 +47,11 @@
     <!-- Jquery JS -->
     <script src="{{ asset('plugins/jquery/jquery.min.js') }}"></script>
     <!-- Bootstrap-->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+    <!-- AdminLTE APP -->
+    <script src="{{ asset('dist/js/adminlte.min.js') }}"></script>
+    <!-- Custom JS -->
+    @yield('scripts')
 </body>
 
 </html>
